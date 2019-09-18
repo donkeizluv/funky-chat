e2e19ba87ccbd49ea7e26dfbf2d6b5541953786e
Luu Hong <donkeizluv@gmail.com>
2019-09-18 22:56:56 +0700

    fix socketio path

--- src/utility/socket-instance.js
+++ src/utility/socket-instance.js
@@ -1,6 +1,2 @@
 import io from "socket.io-client";
-export default io(
-  process.env.VUE_APP_ENV === "prod"
-    ? process.env.VUE_APP_BASE_URI
-    : "localhost:8000"
-);
+export default io(process.env.VUE_APP_BASE_URI);
